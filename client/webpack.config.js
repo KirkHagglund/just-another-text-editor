0858ce775ca25fda33ab4cd62ae8ce7bd7f69e40
elkirko <kirkhagglund@gmail.com>
2023-03-21 18:12:45 -0600

    Added webpack-config plugin details

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,34 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Text Editor',
+        description: 'Just another Text Editor',
+        background_color: '#33e0ff',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            ios: true,
+            rename: 'icon-[width].[ext]'
+          },
+        ]
+      }),
     ],
 
     module: {
